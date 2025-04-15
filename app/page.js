c0ca05ce6ed21733f6898fa29e5d3f6c305b7e7c
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-04-16 05:05:16 +0600

    Added React-router-dom, login page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,26 @@
+"use client"
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+
 import { Banner } from "@/components/Banner";
 import { Header } from "@/components/Header";
 import { Separator } from "@/components/ui/separator";
+import { LogIn } from "@/views/logIn";
+
 
 export default function Home() {
     return (
-        <div>
-            <Header></Header>
-            <Separator className="bg-red-600"></Separator>
-            <Banner></Banner>
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header></Header>
+                <Separator className="bg-red-600"></Separator>
+                
+
+                <Routes>
+                    <Route path="/log-in" element={ <LogIn></LogIn>} />
+                    <Route path="/" element={<Banner></Banner>}></Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
+        
     );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,20 @@
+import { Link } from "react-router-dom";
 import { ThemeToggle } from "./ThemeToggle";
 import { Button } from "./ui/button";
 
 export const Header = () => {
     return (
         <div className="flex justify-between px-5 py-5">
-            <h1 className="text-center flex items-center justify-center font-bold text-2xl text-red-600">
-                Bipod Shongket
-            </h1>
+            <Link to="/">
+                <h1 className="text-center flex items-center justify-center font-bold text-2xl text-red-600">
+                    Bipod Shongket
+                </h1>
+            </Link>
+            
             <div className="space-x-2">
-                <Button>Log In</Button>
-                <Button>Register</Button>
-                <Button>About</Button>
+                <Link to="/log-in"><Button >Log In</Button></Link>
+                <Link to="/register"><Button >Register</Button></Link>
+                <Link to="/sbout"><Button >About</Button></Link>
                 <ThemeToggle></ThemeToggle>
             </div>
         </div>
