fdcff29e5cbe03b620e49671fa683bb3bad117fc
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-04-19 08:54:37 +0600

    updated banner with search and card component

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,8 +1,20 @@
-export const Banner = () => {
+import Card from "./Card";
+import { Input } from "./ui/input";
+
+
+
+export default function Banner(){
     return (
-        <div className="flex flex-col text-8xl font-black justify-around items-center my-10">
-            <h1>Bipod</h1>
-            <h1>Shongket</h1>
+        <div className="flex flex-col ">
+            <div className="flex flex-col items-center">
+                <h1 className="text-2xl p-5 font-bold">Search Recent Reports Based on any Area Name</h1>
+                <Input placeholder="Search"className="w-2xl h-8 border-red-700 border-2 rounded-3xl p-4"></Input>
+            </div>
+            <div className=" flex flex-wrap justify-self-start p-3">
+                <Card></Card>
+            </div>
+            
         </div>
+        
     );
 };
