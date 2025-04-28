d5edf3b71bcbb7193cadcae7605f9d62cba68503
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-04-28 09:53:24 +0600

    added map

---
+++ app/mapdash/page.js
@@ -0,0 +1,32 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import "leaflet/dist/leaflet.css";
+
+// Dynamically import MapContainer with SSR disabled
+const MapContainer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.MapContainer),
+  { ssr: false }
+);
+const TileLayer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.TileLayer),
+  { ssr: false }
+);
+
+export default function Mapdash() {
+  const position = [23.685, 90.3563];
+
+  return (
+    <MapContainer
+      center={position}
+      zoom={9}
+      scrollWheelZoom={true}
+      className="h-120 w-full"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
+}
