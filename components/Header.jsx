7cfda773257058c1fdd2f8d5d04d5c184cff810b
Nasif Ahmed <nasif2ahmed@gmail.com>
2025-05-01 14:39:00 +0600

    Add some theme consistency

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,43 +9,47 @@ import { Separator } from "./ui/separator";
 import { UserNav } from "./UserNav";
 
 export default function Header() {
-  const { user } = useContext(AuthContext);
-  return (
-    <>
-      <div className="flex justify-between items-center px-5 py-5">
-        <Link href="/">
-          <h1 className="text-center flex items-center justify-center font-bold text-2xl text-red-600">
-            Bipod Shongket
-          </h1>
-        </Link>
+    const { user } = useContext(AuthContext);
+    return (
+        <>
+            <div className="flex justify-between items-center px-5 py-5">
+                <Link href="/">
+                    <h1 className="text-center flex items-center justify-center font-bold text-2xl text-red-600">
+                        Bipod Shongket
+                    </h1>
+                </Link>
 
-        <div className="space-x-2 flex justify-around items-center">
-          {user ? (
-            <UserNav />
-          ) : (
-            <>
-              <Link href="/log-in">
-                <Button>Log In</Button>
-              </Link>
-              <Link href="/register">
-                <Button>Register</Button>
-              </Link>
-            </>
-          )}
-          <Link href="/allsosrequest">
-            <Button className="bg-red-600">See All Global Request</Button>
-          </Link>
-          <Link href="/mapdash">
-            <Button className="bg-red-600">MAP</Button>
-          </Link>
-          <Link href="/about">
-            <Button>About</Button>
-          </Link>
-          <ThemeToggle></ThemeToggle>
-        </div>
-      </div>
+                <div className="space-x-2 flex justify-around items-center">
+                    {user ? (
+                        <UserNav />
+                    ) : (
+                        <>
+                            <Link href="/log-in">
+                                <Button>Log In</Button>
+                            </Link>
+                            <Link href="/register">
+                                <Button>Register</Button>
+                            </Link>
+                        </>
+                    )}
+                    <Link href="/allsosrequest">
+                        <Button className="bg-red-700 text-secondary-foreground">
+                            See All Global Request
+                        </Button>
+                    </Link>
+                    <Link href="/mapdash">
+                        <Button className="bg-red-700 text-secondary-foreground">
+                            MAP
+                        </Button>
+                    </Link>
+                    <Link href="/about">
+                        <Button>About</Button>
+                    </Link>
+                    <ThemeToggle></ThemeToggle>
+                </div>
+            </div>
 
-      <Separator className="bg-red-600"></Separator>
-    </>
-  );
+            <Separator className="bg-red-600"></Separator>
+        </>
+    );
 }
