87c898a0fadfc33ec9e7dcee172c243cdd84fa34
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-05-05 21:56:01 +0600

    removed card

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,21 +1,11 @@
-import MyCard from "./MyCard";
-import { Input } from "./ui/input";
-
 export default function Banner() {
-    return (
-        <div className="flex flex-col ">
-            <div className="flex flex-col items-center">
-                <h1 className="text-2xl p-5 font-bold">
-                    Search Recent Reports Based on any Area Name
-                </h1>
-                <Input
-                    placeholder="Search"
-                    className="w-2xl h-8 border-red-700 border-2 rounded-3xl p-4"
-                ></Input>
-            </div>
-            <div className=" flex flex-wrap justify-self-start p-3">
-                <MyCard></MyCard>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col ">
+      <div className="flex flex-col items-center">
+        <h1 className="text-2xl p-5 font-bold">
+          Our Latest Summary Made with the top ten most Recent Posts
+        </h1>
+      </div>
+    </div>
+  );
 }
